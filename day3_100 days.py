82ca530b4b1aa535c65daedd6f2fad96a8d73527
SCS Balaji <150114634+SCSBalaji@users.noreply.github.com>
2024-09-07 19:24:53 +0530

    Add day3_100.py to the repo

---
+++ day3_100 days.py
@@ -0,0 +1,23 @@
+food = input("What is your favorite food? ")
+plant = input("What is your favorite plant? ")
+method = input("What is the method of cooking? ")
+burned = input("What word describes burned food? ")
+item = input("Name a household item: ")
+print()
+print("MENU")
+print(method, food, "with", burned, plant, "on", item, "is worst")
+
+
+"""
+food = input("Name a type of food: ")
+plant = input("Name a plant: ")
+cookingType = input("What is a way to cook something? ")
+burntFood = input("How do you describe burnt food? ")
+householdItem = input("Name something in your house: ")
+
+print()
+print("Tonight's dinner:")
+
+print("For dinner you should make", cookingType, food, "with", burntFood, plant, "on a plate of", householdItem)
+
+"""
